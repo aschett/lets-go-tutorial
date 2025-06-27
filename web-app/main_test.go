49526472a9c5f9d2fa23e69721985e38aa4d306b
aschett <a.schett@outlook.de>
2025-06-27 09:12:30 +0200

    add tests for new routes

--- web-app/main_test.go
+++ web-app/main_test.go
@@ -20,3 +20,35 @@ func TestHome(t *testing.T) {
 		t.Errorf("expected response %q, got %q", expected, rr.Body.String())
 	}
 }
+
+func TestSnippetView(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/snippet/view", nil)
+	rr := httptest.NewRecorder()
+
+	snippetView(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected OK, got %d", rr.Code)
+	}
+
+	expected := "Display specific snippet"
+	if rr.Body.String() != expected {
+		t.Errorf("expected response %q, got %q", expected, rr.Body.String())
+	}
+}
+
+func TestSnippetCreate(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/snippet/create", nil)
+	rr := httptest.NewRecorder()
+
+	snippetCreate(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected OK, got %d", rr.Code)
+	}
+
+	expected := "Display form to create new snippet"
+	if rr.Body.String() != expected {
+		t.Errorf("expected response %q, got %q", expected, rr.Body.String())
+	}
+}
